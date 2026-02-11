d30d53f259f588e7b05da63f5512074ca7af2ed4
Otchy <id+github@otchy.net>
2026-02-11 11:07:50 +0900

    Fix lint command error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,14 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['**/dist/**', '**/coverage/**', '**/node_modules/**', '**/.vite/**', '**/*.d.ts']
+    ignores: [
+      '**/dist/**',
+      '**/coverage/**',
+      '**/node_modules/**',
+      '**/.vite/**',
+      '**/*.d.ts',
+      'eslint.config.mjs'
+    ]
   },
   js.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
